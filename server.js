5f0b6e93e264e5fb28c4b91af9b12d2e17b55159
visa-card-l <danny98765@tutamail.com>
2025-12-01 13:36:07 +0100

    Fix CORS origin for sendmi.onrender.com

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://sendm.onrender.com'],
+  origin: ['http://localhost:3000', 'https://sendmi.onrender.com'],
   credentials: true
 }));
 app.use(express.json());
